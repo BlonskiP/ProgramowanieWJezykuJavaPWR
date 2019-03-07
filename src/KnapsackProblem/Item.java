ff4a4508e23580d1fc368236f464cbd93b1d36ff
Piotr Blonski <piotrblonski96@gmail.com>
2019-03-07 13:39:52 +0100

    Item javadoc

--- src/KnapsackProblem/Item.java
+++ src/KnapsackProblem/Item.java
@@ -1,12 +1,42 @@
 package KnapsackProblem;
-
+/**
+*Represents Item in bag.
+*It has name, value and weight
+*/
 public class Item {
+	/**
+	Name of Item. Example:"Banana"
+	*/
+	private string name;
+	/**
+	Value of Item.
+	This value is used in algorithm to find out which Item sets are best for bag.
+	Example: 50
+	*/
     private float value;
+	/**
+	Weight of Item.
+	Used by algorithm to check if item can fit in bag.
+	Example: 4
+	*/
     private int weight;
-    public Item(float value,int weight){
+	/**
+	Constructor of Item object.
+	@param value a value of item. Cannot be null
+	@param weight a weight of item. Cannot be null
+	@param name a name of item. 
+	*/
+    public Item(float value,int weight, string name){
         this.value=value;
         this.weight=weight;
+		this.name=name;
     }
+	/**
+	@return value of item
+	*/
     public float getValue(){return value;}
+	/**
+	@retrun weight of item
+	*/
     public float getWeight(){return weight;}
 }
