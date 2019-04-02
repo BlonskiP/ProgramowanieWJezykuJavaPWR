4e12660ba60d3de6b600cdbaf82e3d8b62e6f74f
Wilk <piotr.blonski@coderscrew.pl>
2019-04-03 00:40:09 +0200

    trying loader

--- lab02/src/AlgorithClassLoader.java
+++ lab02/src/AlgorithClassLoader.java
@@ -1,18 +1,30 @@
 import KnapsackProblem.KnapsackSolvingAlgorithms.InstanceProblem;
 import KnapsackProblem.KnapsackSolvingAlgorithms.KnapsackSolvingResult;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
-public class AlgorithClassLoader extends ClassLoader{
+
+public class AlgorithClassLoader{
     public Method solve;
     private Object solverObject;
 
     public AlgorithClassLoader(String classBinName, InstanceProblem problem)
     {
         try{
-            ClassLoader classLoader=this.getClass().getClassLoader();
+            String path = new File(AlgorithClassLoader.class.getProtectionDomain().getCodeSource().getLocation()
+                    .toURI()).getPath();
+            path=path+"\\classes";
+            File classFile=new File(path);
+            URL[] urls = new URL[]{classFile.toURI().toURL()};
+            ClassLoader classLoader=new URLClassLoader(urls);
             Class<?> loadedMyClass = classLoader.loadClass(classBinName);
             Constructor[] allConstructors = loadedMyClass.getDeclaredConstructors();
             solverObject = allConstructors[0].newInstance(problem);
